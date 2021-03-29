cdcc843d5442ad670c246452e83db647b91198d4
Ceinmark <Ceinmark@DESKTOP-J0PN6TT>
2021-03-29 10:38:30 +0200

    Version inicial(menu)

--- src/tercerTrim/Restaurante.java
+++ src/tercerTrim/Restaurante.java
@@ -1,8 +1,40 @@
 package tercerTrim;
 
+import java.util.Scanner;
+
 public class Restaurante {
 
 	public static void main (String args[]) {
+		
+		Scanner ent = new Scanner(System.in);
+		int elec = 0;
+		 
+		System.out.println("//////////////////////////////////////////////////////////////////////////\r\n" + 
+				"// Bienvenido a nuestro restaurante, estaremos encantados de atenderle 	//\r\n" + 
+				"// ___________________________________________________________________  //\r\n" + 
+				"// ¿Qué desea hacer?						       	//\r\n" + 
+				"// 1.Reservar una mesa						   	//\r\n" + 
+				"// 2.Realizar pedido						   	//\r\n" + 
+				"// 3.Proceder con el pago					   	// \r\n" + 
+				"//////////////////////////////////////////////////////////////////////////");
+		
+		elec = ent.nextInt();
+		
+		switch (elec) {
+		
+		case 1:
+			//parte del codigo correspondiente a ReservaMesa();
+			
+			break;
+		case 2:
+			//parte del codigo correspondiente a RealizarPedido();
+			
+			break;
+		case 3:
+			//parte del codigo correspondiente a cobroCliente()$;
+			
+			break;
+		}
 		
 	}
 }
