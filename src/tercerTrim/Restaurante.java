37583195b5771b973a2476ab30bcfbf85799602e
CARLOS <CARLOS@DESKTOP-O5UC6IA.home>
2021-03-29 21:47:44 +0200

    actualizacion

--- src/tercerTrim/Restaurante.java
+++ src/tercerTrim/Restaurante.java
@@ -41,12 +41,12 @@ public class Restaurante {
 
 	public static void RealizarPedido() {// Parte de CarlosAntonyMoscosoPalacios
 		Scanner ent = new Scanner(System.in);
-		
+		boolean op = true;
+
 		System.out.println("¿Cuantas personas van a pedir?");
 		int el = ent.nextInt();
-		int cont =1;
-		
-		
+		int cont = 1;
+
 		// Primer Menu
 		int Menu = 1;
 		String Primero = "Lentejas con chorizo", Segundo = "Chuleton con patatas", Postre = "Tarta de queso";
@@ -55,44 +55,56 @@ public class Restaurante {
 		int Menu2 = 2;
 		String Primero2 = "Cocido Montañes", Segundo2 = "Pescado con ensalada", Postre2 = "Natillas";
 		float precio2 = 12;
-		//Tercer menu
+		// Tercer menu
 		int Menu3 = 3;
 		String Primero3 = "Macarrones con queso", Segundo3 = "Hamburguesa con patatas", Postre3 = "Helado";
 		float precio3 = 13.50f;
-		
-		
+
 		System.out.println("******LA CARTA******");
-		 
-		 System.out.println("*************************" +"\n"+"Menu: "  + Menu + "\n" + "Primer Plato: " + Primero + "\n" + "Segundo Plato: " + Segundo + "\n" + 
-				 "Postre: " + Postre + "\n" + "Precio: " + precio + "\n" + "*************************");
-		 
-		 System.out.println("Menu: "  + Menu2 + "\n" + "Primer Plato: " + Primero2 + "\n" + "Segundo Plato: " + Segundo2 + "\n" + 
-				 "Postre: " + Postre2 + "\n" + "Precio: " + precio2 + "\n" + "*************************");
-		 
-		 System.out.println("Menu: "  + Menu3 + "\n" + "Primer Plato: " + Primero3 + "\n" + "Segundo Plato: " + Segundo3 + "\n" + 
-				 "Postre: " + Postre3 + "\n" + "Precio: " + precio3 + "\n" + "*************************");
-		 do {
-		 System.out.println("Que menu desea escoger :" + "\n");
-		 int el2 = ent.nextInt();
-		 
-		 switch(el2) {
-		 
-		 case 1:
-			 System.out.println("*************************" +"\n"+"Menu: "  + Menu + "\n" + "Primer Plato :" + Primero + "\n" + "Segundo Plato :" + Segundo + "\n" + 
-					 "Postre :" + Postre + "\n" + "Precio :" + precio + "\n" + "*************************");
-			 break;
-		 case 2:
-			 System.out.println("Menu: "  + Menu2 + "\n" + "Primer Plato :" + Primero2 + "\n" + "Segundo Plato :" + Segundo2 + "\n" + 
-					 "Postre :" + Postre2 + "\n" + "Precio :" + precio2 + "\n" + "*************************");
-			 break;
-		 case 3:
-			 System.out.println("Menu: "  + Menu3 + "\n" + "Primer Plato :" + Primero3 + "\n" + "Segundo Plato :" + Segundo3 + "\n" + 
-					 "Postre :" + Postre3 + "\n" + "Precio :" + precio3 + "\n" + "*************************");
-			 break;
-		
-		 }
-		 cont++;
-		}while(cont <= el);
+
+		System.out.println("*************************" + "\n" + "Menu: " + Menu + "\n" + "Primer Plato: " + Primero
+				+ "\n" + "Segundo Plato: " + Segundo + "\n" + "Postre: " + Postre + "\n" + "Precio: " + precio + "\n"
+				+ "*************************");
+
+		System.out.println("Menu: " + Menu2 + "\n" + "Primer Plato: " + Primero2 + "\n" + "Segundo Plato: " + Segundo2
+				+ "\n" + "Postre: " + Postre2 + "\n" + "Precio: " + precio2 + "\n" + "*************************");
+
+		System.out.println("Menu: " + Menu3 + "\n" + "Primer Plato: " + Primero3 + "\n" + "Segundo Plato: " + Segundo3
+				+ "\n" + "Postre: " + Postre3 + "\n" + "Precio: " + precio3 + "\n" + "*************************");
+
+		do {
+			System.out.println("Que menu desea escoger :" + "\n");
+			int el2 = ent.nextInt();
+			
+			
+			if(el2 == 1 || el2 == 2 ||el2 == 3 && el !=0) {
+				switch (el2) {
+
+				case 1:
+					System.out.println("*************************" + "\n" + "Menu: " + Menu + "\n" + "Primer Plato :"
+							+ Primero + "\n" + "Segundo Plato :" + Segundo + "\n" + "Postre :" + Postre + "\n"
+							+ "Precio :" + precio + "\n" + "*************************");
+					break;
+				case 2:
+					System.out.println("Menu: " + Menu2 + "\n" + "Primer Plato :" + Primero2 + "\n" + "Segundo Plato :"
+							+ Segundo2 + "\n" + "Postre :" + Postre2 + "\n" + "Precio :" + precio2 + "\n"
+							+ "*************************");
+					break;
+				case 3:
+					System.out.println("Menu: " + Menu3 + "\n" + "Primer Plato :" + Primero3 + "\n" + "Segundo Plato :"
+							+ Segundo3 + "\n" + "Postre :" + Postre3 + "\n" + "Precio :" + precio3 + "\n"
+							+ "*************************");
+					break;
+
+				}
+				cont++;
+			}else {
+				System.out.println("Por favor introduce el Menu entre 1 al 3, gracias");
+			}
+			
+
+		} while (cont <= el);
+
 	}
 
 }
